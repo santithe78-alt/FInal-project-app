9dc4626ec0c3a7259a7a44755946be1210920820
santithe78-alt <santithe78@gmail.com>
2025-11-25 18:13:33 +0000

    Global Var added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("helloooo")
+let currWord = getRandomWord();
 
 function getRandomWord(){
     let movies = ['ZombieLand', 'JohnWick', 'titanic', 'matrix']
@@ -7,18 +8,17 @@ function getRandomWord(){
     return randomWord;
 }
 
-function createBlanks() {
-    const words = getRandomWord();
-    const len = words.length;
-    return len;
-}
 
-console.log(createBlanks())
-console.log(getRandomWord())
+console.log(currWord)
+console.log(currWord.length)
 
 
 
 function keyClick(event) {
     let letter = event.target.innerText;
-console.log(letter)
+    console.log(letter)
+}
+
+function checkLetter(letter) {
+    //see if the keyboard letter is the same as the word in the global var
 }
