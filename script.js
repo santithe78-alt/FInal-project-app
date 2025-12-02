999e535f1b3a94bf88b42b9bee23b694ab86f02a
santithe78-alt <santithe78@gmail.com>
2025-12-02 17:39:37 +0000

    Words connect to Words

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("helloooo")
 let currWord = getRandomWord();
 
 function getRandomWord(){
-    let movies = ['ZombieLand', 'JohnWick', 'titanic', 'matrix']
+    let movies = ['zombieland', 'john wick', 'titanic', 'matrix']
     let randomIndex = Math.floor(Math.random() * movies.length);
     let randomWord = movies[randomIndex]
     return randomWord;
@@ -17,8 +17,9 @@ console.log(currWord.length)
 function keyClick(event) {
     let letter = event.target.innerText;
     console.log(letter)
+    checkLetter(letter.toLowerCase());
 }
 
 function checkLetter(letter) {
-    //see if the keyboard letter is the same as the word in the global var
+    console.log(currWord.indexOf(letter));
 }
